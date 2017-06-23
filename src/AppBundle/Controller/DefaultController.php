caeea8d872ba7c35ffccff38c7614dba6daa6dc6
Serghei Luchianenco <luchianenco@auxmoney.com>
2017-06-23 20:34:34 +0200

    webhook route added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use AppBundle\Service\BotClientService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -19,6 +20,25 @@ class DefaultController extends Controller
     {
         //Bot Client should be bot client service alias
         $data = $service->run($botClient);
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/client/facebook/webhook", name="facebook_webhook")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function webhookAction(Request $request)
+    {
+        $data = [];
+        $verifyToken = $this->getParameter('facebook_verify_token');
+
+        if ($request->query->get('hub_mode') == 'subscribe') {
+            if ($request->query->get('hub_verify_token') == $verifyToken) {
+                $data = $request->query->get('hub_challenge');
+            }
+        }
 
         return new JsonResponse($data);
     }
