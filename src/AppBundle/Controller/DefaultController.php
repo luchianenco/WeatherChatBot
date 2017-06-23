0cb1e233f01c2f708be4debb60d5887183832e3c
Serghei Luchianenco <luchianenco@auxmoney.com>
2017-06-23 21:08:19 +0200

    webhook route update

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Service\BotClientService;
-use Buzz\Message\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
