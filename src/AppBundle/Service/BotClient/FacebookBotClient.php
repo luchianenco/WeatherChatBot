3d2925a6bc509ce4da9d8db43965118fe57133a9
Serghei Luchianenco <luchianenco@auxmoney.com>
2017-06-23 23:37:06 +0200

    FacebookBotClient updated, services updated

--- src/AppBundle/Service/BotClient/FacebookBotClient.php
+++ src/AppBundle/Service/BotClient/FacebookBotClient.php
@@ -1,7 +1,9 @@
 <?php
 
-
 namespace AppBundle\Service\BotClient;
+
+use AppBundle\Model\BotResponse\FacebookBotResponse\FacebookResponseInterface;
+use Buzz\Browser;
 
 /**
  * Facebook Bot Client
@@ -9,12 +11,46 @@ namespace AppBundle\Service\BotClient;
  */
 class FacebookBotClient implements BotClientInterface
 {
-    public function __construct()
+    const FB_URL = 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token=PAGE_ACCESS_TOKEN';
+
+    /**
+     * @var string
+     */
+    private $accessToken;
+
+    /**
+     * @var mixed
+     */
+    private $url;
+
+    /**
+     * @var Browser
+     */
+    private $browser;
+
+    // TODO remove direct dependancy on Browser class
+    // TODO make abstract layer
+    public function __construct(Browser $browser, $accessToken)
     {
+        $this->browser = $browser;
+        $this->accessToken = $accessToken;
+        $this->url = str_replace('PAGE_ACCESS_TOKEN', $this->accessToken, self::FB_URL);
     }
 
     public function run()
     {
         return 'facebook';
+    }
+
+
+    /**
+     * @param FacebookResponseInterface $response
+     * @return bool
+     */
+    public function send(FacebookResponseInterface $response)
+    {
+        $response = $this->browser->post($this->url, $response->formatJsonResponse());
+
+        return json_decode($response);
     }
 }
